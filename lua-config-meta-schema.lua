0c2ed352cd7f4ee21fd30d20874e4ffaf423669d
Alan M. Carroll <amc@apache.org>
2017-12-23 10:41:54 -0600

    Update TsLuaConfig meta schema.

--- lua-config-meta-schema.lua
+++ lua-config-meta-schema.lua
@@ -1,11 +1,12 @@
+schema =
 {
-   ['$scheme']= "http://trafficsever.apache.org/config/schema",
+   ['$schema']= "http://trafficsever.apache.org/config/meta-schema",
    description = "Lua Configuration MetaSchema",
    global = 'schema',
    class = 'TsLuaMetaConfig',
-   type = "object",
+
    properties = {
-      ["$schema"] = {
+      ['$schema'] = {
          type= "string",
          description= "Schema identifier."
       },
@@ -21,12 +22,7 @@
          type = 'string',
          description = 'C++ class for configuration.'
       },
-      type = {
-         type= ='string',
-         enum = { 'null', 'boolean', 'object', 'array', 'number', 'string', 'enum' }
-      },
-      properties={
-         variant = {'type', 'object' },
+      properties = {
          type = 'object',
          description = 'The members of the object.',
          additionalProperties = true,
@@ -34,12 +30,20 @@
          properties = {}
       },
       items = {
-         variant = {'type', 'array'},
-         type = 'array',
+         type = 'object',
          description = 'The items in the array.',
          additionalItems = true,
          minItems = 1,
          items = {}
+      },
+      dependencies = {
+         type = OBJECT,
+         description = 'List of optional properties for an object.'
+      },
+      one_of = {
+         type = OBJECT,
+         description = 'Set of mutually exclusive properties, exactly one of which must be present.',
+         items = STRING
       },
       definitions = {
          type = 'array',
@@ -48,6 +52,121 @@
             type = 'object',
             description = 'Description of the definition.',
             additionalProperties = true
+         }
+      }
+   },
+   dependencies = {
+      properties = {
+         ['not'] = {
+            required = { 'items' }
+         },
+      items = {
+         ['not'] = {
+            required = { 'properties' }
+         }
+      }
+   },
+   definitions = {
+      object = {
+        class = {
+         type = STRING,
+         description = 'C++ struct name.'
+        },
+        description = {
+         type = STRING,
+         description = 'Object description.'
+        },
+        properties = {
+            ['$ref'] = '#/definitions/object'
+        }
+      },
+      --[[
+         It should be possible to specify how to set up an enumeration with just an array,
+         filling the other values with defaults. E.g. an array of strings or an array of integers.
+      ]]
+      enum = {
+         type = OBJECT,
+         typeName = 'EnumType',
+         description = 'Enumeration',
+         properties = {
+            typeName = {
+               type = 'string',
+               description = 'C++ type name.'
+            },
+            global = {
+               type = STRING,
+               description = 'Global variable that contains the enumeration values.'
+            },
+            kv = {
+               type = ARRAY,
+               description = 'Enumeration keys and values',
+               items = {
+                  type = 'object',
+                  description = 'Enumeration value',
+                  properties = {
+                     key = {
+                        type = 'string',
+                        description = 'Enumeration key'
+                     },
+                     value = {
+                        type = 'integer',
+                        description = 'Enumeration value'
+                     },
+                     description = {
+                        type = 'string',
+                        description = 'Meaning of this value'
+                     }
+                  }
+               }
+            }
+         }
+      },
+      ValueType = {
+         type = ENUM,
+         typeName = 'ValueType',
+         description = 'Type of data',
+         global = '.',
+         kv = {
+            {
+               key = 'nil',
+               value = 0,
+               description = 'Null / invalid value.'
+            },
+            {
+               key = 'boolean',
+               value = 1,
+               description = 'Boolean (true/false) value.'
+            },
+            {
+               key = 'string',
+               value = 2,
+               description = 'String value.'
+            },
+            {
+               key = 'integer',
+               value = 3,
+               description = 'Integral value.'
+            },
+            {
+               key = 'number',
+               value = 4,
+               description = 'Numeric value.'
+            },
+            {
+               key = 'object',
+               value = 5,
+               description = 'Object - collection of key / value pairs.'
+            },
+            {
+               key = 'array',
+               value = 6,
+               description = 'Array of values.'
+            },
+            {
+               key = 'enum',
+               value = 7,
+               description = 'Enumeration'
+            }
          }
       }
    }
